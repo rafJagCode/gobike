4355f8761217dbaae29331c061253d768f3a86e4
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2021-01-22 20:06:44 +0100

    fixed script problem

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,6 +11,6 @@
     <body>
         <div id="app">
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script type=text/javascript src="js/app.js"></script>
     </body>
 </html>
